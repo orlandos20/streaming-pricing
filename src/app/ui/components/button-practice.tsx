471336a528972368bd2834809ae2f9574d23bee2
Orlando Jimenez <orlandojn10@gmail.com>
2026-01-15 18:10:57 +0100

    chore: install ui dependencies and a test component

---
+++ src/app/ui/components/button-practice.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+export function ButtonPractive() {
+  return (
+    <Button onClick={() => console.log('Button clicked!')}>
+      ShadCN Button
+    </Button>
+  );
+}
